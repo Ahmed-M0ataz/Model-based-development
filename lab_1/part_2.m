d823e85515bd590f92e72a615eb09067e99aa239
Ahmed-M0ataz <91518@must.edu.eg>
2023-10-07 21:47:32 +0300

    feat: My first feature

---
+++ lab_1/part_2.m
@@ -0,0 +1,7 @@
+clear
+clc
+
+rowVector = 1:5;
+columnVector = (6:10)';
+
+horizontalConcat = cat(2,rowVector,columnVector')
---
+++ lab_1/part_4.m
@@ -0,0 +1,6 @@
+%clear
+clc
+
+identityMatrix = eye(3,3);
+
+matrixProduct = identityMatrix * matrixA
